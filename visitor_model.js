911a07e12542d2c7fafd7f2753cdb4c6795c4bcd
Nabil Abbar <71392347+ABBARNABIL@users.noreply.github.com>
2022-09-13 16:37:32 +0200

    Update visitor_model.js

--- visitor_model.js
+++ visitor_model.js
@@ -1,15 +1,17 @@
-const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DB,
-  password: process.env.PG_PASSWORD,
-  port: 5432,
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+client.connect();
 
 const getVisitors = () => {
   return new Promise(function (resolve, reject) {
-    pool.query("SELECT * FROM visitors ORDER BY id ASC", (error, results) => {
+    client.query("SELECT * FROM visitors ORDER BY id ASC", (error, results) => {
       if (error) {
         reject(error);
       }
@@ -21,7 +23,7 @@ const getVisitors = () => {
 const createVisitor = (body) => {
   return new Promise(function (resolve, reject) {
     const { name, email } = body;
-    pool.query(
+    client.query(
       "INSERT INTO visitors (name, email) VALUES ($1, $2) RETURNING *",
       [name, email],
       (error, results) => {
@@ -36,12 +38,16 @@ const createVisitor = (body) => {
 
 const deleteVisitor = (id) => {
   return new Promise(function (resolve, reject) {
-    pool.query("DELETE FROM visitors WHERE id = $1", [id], (error, results) => {
-      if (error) {
-        reject(error);
+    client.query(
+      "DELETE FROM visitors WHERE id = $1",
+      [id],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(`Visitor deleted with ID: ${id}`);
       }
-      resolve(`Visitor deleted with ID: ${id}`);
-    });
+    );
   });
 };
 
