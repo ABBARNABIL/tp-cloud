ac1bc46e930ca1b672bf9010b3491283d0ccda47
Nabil Abbar <71392347+ABBARNABIL@users.noreply.github.com>
2022-09-13 15:26:26 +0200

    Update visitor_model.js

--- visitor_model.js
+++ visitor_model.js
@@ -1,9 +1,9 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "my_user",
-  host: "localhost",
-  database: "postgres",
-  password: "root",
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASSWORD,
   port: 5432,
 });
 
